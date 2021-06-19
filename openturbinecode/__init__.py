94b109b2af2ccd60f21ac295667397dff3b37acd
marcomangano <mmangano@umich.edu>
2021-06-19 18:35:49 -0400

    more import check

--- openturbinecode/__init__.py
+++ openturbinecode/__init__.py
@@ -1,8 +1,15 @@
+import subprocess
+
 failed_imports = []
-try:
+try: # check adflow import
     import adflow
 except ImportError as err:
     failed_imports.append("adflow")
+
+try:  # check local installation of openfast
+    subprocess.run(['openfast', '--help'], shell=True, check=True)
+except subprocess.CalledProcessError as err:
+    failed_imports.append("openfast")
 
 if len(failed_imports) > 0:
     print("Warning - The following packages are not installed:", failed_imports)
