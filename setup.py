5b1f8e99637ab3b2c90e449fef3bc90441909788
marcomangano <mmangano@umich.edu>
2021-06-21 17:59:29 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="OpenTurbineCoDe",
-    version="0.0.1",
+    version="0.0.2",
     author="",
     author_email="",
     description="Open Turbine Control co-Design package",
