dca38e01e2b8389e0bdb86a2b0d3b5c918ed5df0
Xianping Du <38188001+Seager1989@users.noreply.github.com>
2021-07-04 18:00:27 -0400

    Update the GUI design and driver

--- openturbinecode/controls/DU_ROSCO_v3.py
+++ openturbinecode/controls/DU_ROSCO_v3.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct  2 14:12:35 2020
-
+# The CCD GUI for the framework
 @author: Xianping Du (xianping.du@gmail.com)
 """
 # Config program using PyQt5
@@ -16,47 +16,43 @@ import subprocess
 import scp
 import pandas as pd
 
-form_class = uic.loadUiType("ConfigRosco_v3.ui")[0]  # Load the UI
+form_class = uic.loadUiType("ConfigControl_v3.ui")[0]  # Load the UI
 
 class Mapper(QtWidgets.QMainWindow, form_class):
     def __init__(self,  parent=None):
         QtWidgets.QMainWindow.__init__(self, parent)
         self.setupUi(self)
         # Bind the event handlers to the buttons using a function
-        self.toolButton_2.clicked.connect(self.setyamlWorkingDir)
-        self.saveButton.clicked.connect(self.saveFileDialog)
-        self.runButton.clicked.connect(self.runexample)
-        self.SendToHPC.clicked.connect(self.SendToHPCf)
-        #self.HPCRun_2.clicked.connect(self.HPCloginf)
-        #self.HPCRun.clicked.connect(self.HPCRunf)
-        self.LoadResults.clicked.connect(self.HPCloadf)
-        self.Plotresults.clicked.connect(self.Plotresultsf)
+        # Parametric Sweep
+        self.toolButton_6.clicked.connect(self.setyamlWorkingDir)
+        self.ControlTune.clicked.connect(self.RunRoscoTune)
+        self.PushRun.clicked.connect(self.LocalRun)
+        #self.saveButton.clicked.connect(self.saveFileDialog)
+        self.SendToHPC_2.clicked.connect(self.SendToHPCf)
+        self.LoadResults_2.clicked.connect(self.HPCloadf)
+        #self.QMessageBox .clicked.connect(self.Message)
+        # Optimization
+        self.PushRunCCD.clicked.connect(self.RunCCD)
         
         
-        #set placeholder
-        self.lineEdit.setText("-1028.5")
-        self.lineEdit_2.setText("-185.79")
-        self.lineEdit_6.setText("0.4499")
-        self.lineEdit_7.setText("0.0")
-        self.lineEdit_13.setText("xd101")
-        self.lineEdit_14.setText("amarel.rutgers.edu")
-        self.lineEdit_15.setText("/scratch/xd101/Subroutine-ROSCODemo")
-        self.Responses.setText("GenPwr")
-        self.lineEdit_16.setText("/home/seager/Desktop/APAR_E/GUI/Control/5MW_Land_BD_DLL_WTurb")
-        self.lineEdit_17.setText("/home/seager/Desktop/APAR_E/GUI/Control/5MW_Land_BD_DLL_WTurb/Rosco_tuning/DISCON1.IN")
-    
-    def openFileDialogue(self):  #working directory of the model
-        self.modelFolder = str(QtWidgets.QFileDialog.getExistingDirectory())
-        self.lineEdit_16.setText(self.modelFolder)
+        #set placeholder default text for parametric sweep
+        self.lineEdit_32.setText("0.2")
+        self.lineEdit_31.setText("0.7")
+        self.lineEdit_35.setText("0.3")
+        self.lineEdit_36.setText("0.7")
+        self.lineEdit_27.setText("xd101")
+        self.lineEdit_26.setText("amarel.rutgers.edu")
+        self.lineEdit_24.setText("/scratch/xd101/Subroutine-ROSCODemo")
+        #Get user inputs
+        self.Baselinemodel=self.ModelSelection.text()
         
     def setyamlWorkingDir(self):   #load the control parameters txt file
         #self.ctrldir = str(QtWidgets.QFileDialog.getExistingDirectory())
-        (filePath, fileType) = QtWidgets.QFileDialog.getOpenFileName()
-        self.lineEdit_17.setText(str(filePath))
+        self.yamlfilePath = QtWidgets.QFileDialog.getOpenFileName()
+        self.lineEdit_17.setText(str(self.yamlfilePath))
 
-    def saveFileDialog(self):
-        #fname1 = open('./5MW_Land_BD_DLL_WTurb/Rosco_tuning/DISCON1.IN', 'r')  #newline='\r\n'
-        #fname = open('./5MW_Land_BD_DLL_WTurb/Rosco_tuning/DISCON.IN', 'w')
+    def RunRoscoTune(self):
+        # Tune the ROsco controller and also tune the simulink model
         fname1 = open(self.lineEdit_17.text(), 'r')  #newline='\r\n'
         fname = open(self.lineEdit_16.text()+'/Rosco_tuning/DISCON.IN', 'w')
         for x in list(range(0,22)):
@@ -80,7 +76,7 @@ class Mapper(QtWidgets.QMainWindow, form_class):
         fname.close()
 
     
-    def runexample(self):
+    def LocalRun(self):
         subprocess.run(["openfast", self.lineEdit_16.text()+"/5MW_Land_BD_DLL_WTurb.fst"])
         #os.chdir("testCase")
         #subprocess.run(["pimpleFoam"])
@@ -89,25 +85,20 @@ class Mapper(QtWidgets.QMainWindow, form_class):
     def SendToHPCf(self):
         subprocess.run(["scp", self.lineEdit_16.text()+'/Rosco_tuning/DISCON.IN',self.lineEdit_13.text()+"@"+self.lineEdit_14.text()+":"+self.lineEdit_15.text()+'/5MW_Land_BD_DLL_WTurb/Rosco_tuning'])
         
-    #def HPCloginf(self):
-        #subprocess.run(["ssh", self.lineEdit_13.text()+"@"+self.lineEdit_14.text()])
-        
-    #def HPCRunf(self):
-        #subprocess.run(["ssh", self.lineEdit_13.text()+"@"+self.lineEdit_14.text()])
-        #subprocess.run(["/home/xd101/Downloads/openfast/install/bin/openfast","./5MW_Land_BD_DLL_WTurb/5MW_Land_BD_DLL_WTurb.fst"])
-        
     def HPCloadf(self):
         subprocess.run(["scp", self.lineEdit_13.text()+"@"+self.lineEdit_14.text()+":"+self.lineEdit_15.text()+"/5MW_Land_BD_DLL_WTurb/5MW_Land_BD_DLL_WTurb.out",self.lineEdit_16.text()])
         
-    def Plotresultsf(self):
-        result = numpy.loadtxt("./5MW_Land_BD_DLL_WTurb/5MW_Land_BD_DLL_WTurb.out", skiprows=8)   
-        plt.plot(result[:,0], result[:,62])
-        plt.xlabel('Time (s)', fontsize=16)
-        plt.ylabel('Generator Power (kW)', fontsize=16)
-        plt.show()
+    def RunCCD(self):
+        print('abc')
+        # Collect information
+        # construct the OPT path
+        # CCD based on AutoCCD
+        # Show to progress bar
+        #self.progressBar.setValue(idx/nTotal*100)  #used in the CCD function
 
-app = QtWidgets.QApplication(sys.argv)
-myWindow = Mapper()
-myWindow.show()
-app.exec_()
+if __name__=='__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    myWindow = Mapper()
+    myWindow.show()
+    app.exec_()
 
