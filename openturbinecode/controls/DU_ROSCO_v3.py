625a03c2cba20608d3027a49cdeb926f2dd2255a
Xianping Du <38188001+Seager1989@users.noreply.github.com>
2021-07-05 15:05:09 -0400

    Update the GUI design and modify driver structures with function call place holders.

--- openturbinecode/controls/DU_ROSCO_v3.py
+++ openturbinecode/controls/DU_ROSCO_v3.py
@@ -22,6 +22,68 @@ class Mapper(QtWidgets.QMainWindow, form_class):
     def __init__(self,  parent=None):
         QtWidgets.QMainWindow.__init__(self, parent)
         self.setupUi(self)
+              
+        #set placeholder default text for parametric sweep
+        self.lineEdit_32.setText("0.2")
+        self.lineEdit_31.setText("0.7")
+        self.lineEdit_35.setText("0.3")
+        self.lineEdit_36.setText("0.7")
+        #self.lineEdit_37.setText("0.7")
+        #self.lineEdit_38.setText("0.7")
+        self.lineEdit_39.setText("11.4")
+        self.lineEdit_27.setText("xd101")
+        self.lineEdit_26.setText("amarel.rutgers.edu")
+        self.lineEdit_24.setText("/scratch/xd101/Subroutine-ROSCODemo")
+        # Optimization: FAST
+        self.lineEdit_29.setText("[0,0.5,1.0]")
+        self.lineEdit_45.setText("[0,0.5,1.0]")
+        self.lineEdit_40.setText("[0 0.5,1.0]")
+        self.lineEdit_30.setText("[[[0,-0.5,-0.5],[0,-0.5,-0.5],[0,-0.5,-0.5]],[[0,0.5,0.5],[0,0.5,0.5],[0,0.5,0.5]]]")
+        # Optimization: TACS
+        self.lineEdit_43.setText("[0,0.5,1.0]")
+        self.lineEdit_46.setText("[0,0.5,1.0]")
+        self.lineEdit_42.setText("[0 0.5,1.0]")
+        self.lineEdit_41.setText("[[[0,-0.5,-0.5],[0,-0.5,-0.5],[0,-0.5,-0.5]],[[0,0.5,0.5],[0,0.5,0.5],[0,0.5,0.5]]]")
+        # Optimization configuration
+        self.lineEdit_23.setText("10")
+        self.lineEdit_28.setText("1e-6")
+    
+        #Get user inputs data
+        self.Baselinemodel = self.ModelSelection.text()
+        self.ROSCOR2Omega = self.lineEdit_32.text()
+        self.ROSCOR2Zeta = self.lineEdit_31.text()
+        self.ROSCOR3Omega = self.lineEdit_35.text()
+        self.ROSCOR3Zeta = self.lineEdit_36.text()
+        self.PlatformP1 = self.lineEdit_37.text()
+        self.PlatformP2 = self.lineEdit_38.text()
+        #self.SimulinkP1 = self.lineEdit_.text()
+        #self.SimulinkP2 = self.lineEdit_.text()
+        self.ControlSelection = self.comboBox_3.text()
+        # Run Simulation
+        self.DLC = self.comboBox.text()
+        self.DLCVelocity = self.lineEdit_39.text()
+        # Run on HPC
+        self.Username = self.lineEdit_27.text()
+        self.Server = self.lineEdit_26.text()
+        self.HPCPath = self.lineEdit_24.text()
+        # Parameterization OpenFAST
+        self.FSChordStations = self.lineEdit_29.text()
+        self.FSTwistStations = self.lineEdit_45.text()
+        self.FSThickStations = self.lineEdit_40.text()
+        self.FSLimits = self.lineEdit_30.text()
+        self.FSObjective = self.comboBox_4.text()
+        # Parameterization TACS
+        self.TSChordStations = self.lineEdit_43.text()
+        self.TSTwistStations = self.lineEdit_46.text()
+        self.TSThickStations = self.lineEdit_42.text()
+        self.TSLimits = self.lineEdit_41.text()
+        self.TSObjective = self.comboBox_5.text()
+        # Optimization
+        self.Optimizer = self.comboBox_2.text()
+        self.Iterations = self.lineEdit_23.text()
+        self.Display = self.comboBox_6.text()
+        self.Tolerane = self.lineEdit_28.text()
+        
         # Bind the event handlers to the buttons using a function
         # Parametric Sweep
         self.toolButton_6.clicked.connect(self.setyamlWorkingDir)
@@ -34,58 +96,36 @@ class Mapper(QtWidgets.QMainWindow, form_class):
         # Optimization
         self.PushRunCCD.clicked.connect(self.RunCCD)
         
-        
-        #set placeholder default text for parametric sweep
-        self.lineEdit_32.setText("0.2")
-        self.lineEdit_31.setText("0.7")
-        self.lineEdit_35.setText("0.3")
-        self.lineEdit_36.setText("0.7")
-        self.lineEdit_27.setText("xd101")
-        self.lineEdit_26.setText("amarel.rutgers.edu")
-        self.lineEdit_24.setText("/scratch/xd101/Subroutine-ROSCODemo")
-        #Get user inputs
-        self.Baselinemodel=self.ModelSelection.text()
-        
     def setyamlWorkingDir(self):   #load the control parameters txt file
         #self.ctrldir = str(QtWidgets.QFileDialog.getExistingDirectory())
-        self.yamlfilePath = QtWidgets.QFileDialog.getOpenFileName()
-        self.lineEdit_17.setText(str(self.yamlfilePath))
+        (filePath, fileType) = QtWidgets.QFileDialog.getOpenFileName()
+        self.lineEdit_25.setText(str(filePath))
+        self.YamlFile=filePath
 
     def RunRoscoTune(self):
-        # Tune the ROsco controller and also tune the simulink model
-        fname1 = open(self.lineEdit_17.text(), 'r')  #newline='\r\n'
-        fname = open(self.lineEdit_16.text()+'/Rosco_tuning/DISCON.IN', 'w')
-        for x in list(range(0,22)):
-            fname.write(fname1.readline())
-        fname.write(self.lineEdit_7.text() + '       ! F_LPFDamping	 - Damping coefficient [used only when F_FilterType = 2] \n')
-        fname.write(self.lineEdit_6.text() + '       ! F_NotchCornerFreq  - Natural frequency of the notch filter, [rad/s] \n')
-        fname1.readline();fname1.readline();
-        for x in list(range(1,8)):
-            fname.write(fname1.readline())
-        fname.write(self.lineEdit_3.currentText() + '       ! PC_GS_angles	 - Gain-schedule table: pitch angles \n')
-        fname.write(self.lineEdit_4.currentText() + '       ! PC_GS_KP	 - Gain-schedule table: pitch controller kp gains \n')
-        fname.write(self.lineEdit_5.currentText() + '       ! PC_GS_KI	 - Gain-schedule table: pitch controller ki gains \n')
-        fname1.readline();fname1.readline();fname1.readline();
-        for x in list(range(1,29)):
-            fname.write(fname1.readline())
-        fname.write(self.lineEdit.text() + '       ! VS_KP  - Proportional gain for generator PI torque controller [1/(rad/s) Nm]. \n')
-        fname.write(self.lineEdit_2.text() + '       ! VS_KI  - Integral gain for generator PI torque controller [1/rad Nm]. \n')
-        fname1.readline();fname1.readline();
-        for x in fname1:
-            fname.write(x)       
-        fname.close()
-
-    
+        # Function to tune the controller and write to specific lcation in yaml
+        if self.ControlSelection == "ROSCO":
+            self.YamlFile
+            # ROSCO tunning
+        elif self.ControlSelection == "Legacy":
+            # Only applicable to simulink
+            self.YamlFile
+            function(SimulinkLegacyTUning)
+        else:
+            raise TypeError("Controller should be specified.")
+            self.textBrowser.append(str("TypeError: Controller should be specified."))
+            
     def LocalRun(self):
+        # Local run the case for parametric study
         subprocess.run(["openfast", self.lineEdit_16.text()+"/5MW_Land_BD_DLL_WTurb.fst"])
-        #os.chdir("testCase")
-        #subprocess.run(["pimpleFoam"])
-        #os.chdir("..")
+
         
     def SendToHPCf(self):
+        # Send to HPC for running
         subprocess.run(["scp", self.lineEdit_16.text()+'/Rosco_tuning/DISCON.IN',self.lineEdit_13.text()+"@"+self.lineEdit_14.text()+":"+self.lineEdit_15.text()+'/5MW_Land_BD_DLL_WTurb/Rosco_tuning'])
         
     def HPCloadf(self):
+        # Load retults from HPC
         subprocess.run(["scp", self.lineEdit_13.text()+"@"+self.lineEdit_14.text()+":"+self.lineEdit_15.text()+"/5MW_Land_BD_DLL_WTurb/5MW_Land_BD_DLL_WTurb.out",self.lineEdit_16.text()])
         
     def RunCCD(self):
