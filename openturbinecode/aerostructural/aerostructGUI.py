431ac4aa420f014e42704e2ae410e4fed5ca95f8
Marco Mangano <mmangano@umich.edu>
2021-07-08 13:15:19 -0400

    standalone gui updated to work with module

--- openturbinecode/aerostructural/aerostructGUI.py
+++ openturbinecode/aerostructural/aerostructGUI.py
@@ -10,6 +10,8 @@ from PyQt5 import QtCore, QtGui, uic, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 import subprocess
 
+import openturbinecode.aerostructural.aerostructural_module as aerostruct
+
 form_class = uic.loadUiType(os.path.dirname( os.path.realpath(__file__) ) +os.sep+ "Config.ui")[0]  # Load the UI
 
 class Mapper(QtWidgets.QMainWindow, form_class):
@@ -25,7 +27,13 @@ class Mapper(QtWidgets.QMainWindow, form_class):
     
 if __name__=='__main__':
     app = QtWidgets.QApplication(sys.argv)
-    myWindow = Mapper()
+
+    path_to_case = "./"
+
+    #empty control object
+    myAeroStruct = aerostruct.Aerostructural(path_to_case)
+
+    myWindow = Mapper(myAeroStruct)
     myWindow.show()
     app.exec_()
 
