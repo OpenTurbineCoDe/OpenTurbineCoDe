06aa61379810ef1a3634170e2daba2adce80cd35
Marco Mangano <mmangano@umich.edu>
2022-01-24 16:40:24 -0500

    WIP split adflow setup

--- openturbinecode/aerostructural/Wrapped_hifi_Analysis.py
+++ openturbinecode/aerostructural/Wrapped_hifi_Analysis.py
@@ -6,17 +6,16 @@ from mpi4py import MPI
 import pickle
 from pprint import pprint
 
-from .SETUP import setup_aerostruct, setup_aerostructprob, setup_tacs, setup_structprob, setup_warping
-
 try:
     from baseclasses import AeroProblem
-    from adflow import ADFLOW
     from multipoint import multiPointSparse
     from pyoptsparse import Optimization, OPT
 except ImportError as err:  # noqa
     _has_adflow = False
 else:
     _has_adflow = True
+
+from .SETUP import setup_adflow, setup_aerostruct, setup_aerostructprob, setup_tacs, setup_structprob, setup_warping
 
 """
 Definition of a decorator to be used on every function that requires the sprcific module
@@ -77,81 +76,6 @@ def HiFiAeroStruct(tsr, Vel, pitch, rho, T, options, optimize=False):
         casename = casename + "_restart"
     ap = AeroProblem(name=casename, V=Vel, T=T, rho=rho, areaRef=areaRef, chordRef=spanRef, evalFuncs=["mx", "fx"])
 
-    AEROSOLVER = ADFLOW
-    gridFile = f"{path_to_case}/ADflow/INPUT/{case_tag}_L{hifimesh}.cgns"
-    CFL = 1.5
-    MGCYCLE = "sg"
-    MGSTART = -1
-    nCycles = 200000
-
-    # TODO: for future maintainability, we might need to take the aeroOptions and ADflow instantiation to a separate file
-    # to be also accessed by the optimization script
-
-    aeroOptions = {
-        # Common Parameters
-        "gridFile": gridFile,
-        "restartFile": restart,
-        "outputDirectory": outputDirectory,
-        # Physics Parameters
-        "equationType": "RANS",
-        "smoother": "DADI",
-        "lowspeedpreconditioner": True,
-        "userotationsa": True,
-        "useqcr": True,
-        "useblockettes": False,
-        "vis2": 0.250,
-        "restrictionrelaxation": 1.0,
-        # Common Parameters
-        "CFL": CFL,
-        "CFLCoarse": CFL,
-        "MGCycle": MGCYCLE,
-        "MGStartLevel": MGSTART,
-        "nCyclesCoarse": 250,
-        "nCycles": nCycles,
-        # Newton-Krylov Parameters
-        "usenksolver": True,
-        "nkswitchtol": 1e-14,
-        "nkinnerpreconits": 2,
-        "nkjacobianlag": 3,
-        "nkouterpreconits": 3,
-        "nkpcilufill": 2,
-        "nksubspacesize": 100,
-        # Approximate Newton-Krylov Parameters
-        "useanksolver": True,
-        "ankswitchtol": 1.0,
-        "anklinearsolvetol": 0.05,
-        "ankcflexponent": 0.5,
-        "ankmaxiter": 60,
-        "ankpcilufill": 2,
-        "nsubiterturb": 10,
-        "ankstepmin": 0.01,
-        "anksecondordswitchtol": 1e-2,
-        # 'ankcfllimit':500.,
-        "anklinresmax": 0.1,
-        # Convergence Parameters
-        "L2Convergence": 1e-11,
-        "L2ConvergenceCoarse": 1e-6,
-        # Adjoint Parameters
-        "adjointL2Convergence": 1e-9,
-        "ADPC": True,
-        "adjointMaxIter": 500000,
-        "adjointSubspaceSize": 500,
-        "ILUFill": 2,
-        "ASMOverlap": 1,
-        "outerPreconIts": 3,
-        # Special
-        "monitorvariables": ["cpu", "resrho", "cl", "cd", "resturb", "cfx", "cmx", "yplus"],
-        # Output files
-        "volumevariables": ["resrho", "resturb"],
-        "surfacevariables": ["cp", "mach", "yplus", "sepsensor", "p", "temp"],
-    }
-
-    if int(hifimesh) >= 2:
-        # Different options for coarser meshes
-        aeroOptions["nkswitchtol"] = 1e-8
-        aeroOptions["anklinresmax"] = 0.9
-        aeroOptions["ankcfllimit"] = 500.0
-
     # ======================================================================
     #         Create multipoint communication object
     # ======================================================================
@@ -165,15 +89,8 @@ def HiFiAeroStruct(tsr, Vel, pitch, rho, T, options, optimize=False):
     #         Create aerostruct solver
     # ======================================================================
 
-    # Create solver
-    CFDSolver = AEROSOLVER(options=aeroOptions, comm=comm)
-
-    # setRotationRate(self, rotCenter, rotRate, cgnsBlocks=None)
-    CFDSolver.setRotationRate([0, 0, 0], [rotRate_z, 0, 0])
-
-    pos = np.array([0.1, 0.25, 0.5, 0.75, 0.9]) * spanRef
-    CFDSolver.addSlices(spanDir, pos, sliceType="absolute")
-    CFDSolver.addLiftDistribution(150, spanDir)
+    gridFile = f"{path_to_case}/ADflow/INPUT/{case_tag}_L{hifimesh}.cgns"
+    CFDSolver = setup_adflow.setup(comm, gridFile, hifimesh, restart, outputDirectory, rotRate_z, spanDir, spanRef)
 
     # ---- IDwarp - Warping setup
     mesh = setup_warping.setup(comm, gridFile)
