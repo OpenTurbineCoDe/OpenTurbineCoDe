cfea4f6361b473dab9d761c8d1d3daee32bcd8ec
calexwright <161058434+calexwright@users.noreply.github.com>
2024-10-22 18:29:45 -0400

    Loading FOCAL1 IEA-15MW Focal1 asbuilt to models
    This adds the IEA-15MW Focal1 as-built from UCF to the repository.
    
    Additional plotter utility functions are added.
    
    Beginning of MVC controller for gui in /views/

--- openturbinecode/aerostructural/Wrapped_lofi_Analysis.py
+++ openturbinecode/aerostructural/Wrapped_lofi_Analysis.py
@@ -10,9 +10,11 @@ import shutil
 #         Filling in the data where we need them in the input files
 # ======================================================================
 
-#take filename in inputDir, replace the list of [value] on [iline], and place it in outputDir
-#mod = 0 (replace the value)|1 (multipy by value)|2 (add value)
+# take filename in inputDir, replace the list of [value] on [iline], and place it in outputDir
+# mod = 0 (replace the value)|1 (multipy by value)|2 (add value)
 # TODO: These functions should be generalized and imported from utils (?)
+
+
 def replaceInFile(filename, inputDir, outputDir, iline, value, mod=0, fmt="  %9.5f"):
     ifi = os.path.join(inputDir, filename)
     ofi = os.path.join(outputDir, filename)
@@ -22,23 +24,25 @@ def replaceInFile(filename, inputDir, outputDir, iline, value, mod=0, fmt="  %9.
     try:
         ifh = open(ifi, "rt")
     except Exception:
-        print('Could not open files: '+ ifh )
+        print('Could not open files: ' + ifh)
         exit(1)
-    
-    l = 0 #index of read file
-    lr = 0 #index of replaced values
+
+    l = 0  # index of read file
+    lr = 0  # index of replaced values
     for line in ifh.readlines():
-        l+=1
+        l += 1
         if l in iline:
             if mod == 1:
-                line = "%9.6E"%(float(line[0:12])*value) + line[11:] #multiply the value in the file
+                # multiply the value in the file
+                line = "%9.6E" % (float(line[0:12])*value) + line[11:]
             elif mod == 2:
-                line = "%9.6E"%(float(line[0:12])+value) + line[11:] #multiply the value in the file
+                # multiply the value in the file
+                line = "%9.6E" % (float(line[0:12])+value) + line[11:]
             else:
-                line = fmt%value[lr] + line[11:]
-            
+                line = fmt % value[lr] + line[11:]
+
             buffer += line
-            lr+=1
+            lr += 1
         else:
             buffer += line
     ifh.close()
@@ -52,45 +56,47 @@ def replaceInFile(filename, inputDir, outputDir, iline, value, mod=0, fmt="  %9.
     ofh.write(buffer)
     ofh.close()
 
-#line and colomn indices start at 1
-#mod = 0 (replace the value)|1 (multipy by value)|2 (add value)
+# line and colomn indices start at 1
+# mod = 0 (replace the value)|1 (multipy by value)|2 (add value)
+
+
 def replaceInFileTable(filename, inputDir, outputDir, iline, icol, value, mod=0, separator=',', EF=False):
     ifi = os.path.join(inputDir, filename)
     ofi = os.path.join(outputDir, filename)
-    
+
     buffer = ""
 
     ifh = []
     try:
         ifh = open(ifi, "rt")
     except Exception:
-        print('Could not open files: '+ ifh )
+        print('Could not open files: ' + ifh)
         exit(1)
-    
-    l = 0 #index of read file
-    lr = 0 #index of replaced values
+
+    l = 0  # index of read file
+    lr = 0  # index of replaced values
     for line in ifh.readlines():
-        l+=1
+        l += 1
         if l in iline:
-            #parse line:
-            linesp = line.replace('\n','').split(separator)
+            # parse line:
+            linesp = line.replace('\n', '').split(separator)
             line = ''
 
             if mod == 1:
-                linesp[icol-1] = "%9.6E"%(float(linesp[icol-1])*value)
+                linesp[icol-1] = "%9.6E" % (float(linesp[icol-1])*value)
             elif mod == 2:
-                linesp[icol-1] = "%9.6E"%(float(linesp[icol-1])+value)
+                linesp[icol-1] = "%9.6E" % (float(linesp[icol-1])+value)
             else:
-                linesp[icol-1] = "%9.6E"%(value[lr])
+                linesp[icol-1] = "%9.6E" % (value[lr])
 
-            #reconstruct the line:
+            # reconstruct the line:
             for j in linesp:
                 line += j + separator
-            line = line[0:-len(separator)] #erase last separator
+            line = line[0:-len(separator)]  # erase last separator
             line += '\n'
 
             buffer += line
-            lr+=1
+            lr += 1
 
             if EF and lr == len(iline):
                 break
@@ -98,15 +104,15 @@ def replaceInFileTable(filename, inputDir, outputDir, iline, icol, value, mod=0,
             buffer += line
     ifh.close()
 
-    #if the file was too short, let's extend it:
-    if mod==0 and lr < len(iline):
-        for ll in range(lr,len(iline)):
+    # if the file was too short, let's extend it:
+    if mod == 0 and lr < len(iline):
+        for ll in range(lr, len(iline)):
             line = ''
-            linesp[icol-1] = "%9.6E"%(value[ll])
+            linesp[icol-1] = "%9.6E" % (value[ll])
 
             for j in linesp:
                 line += j + separator
-            line = line[0:-len(separator)] #erase last separator
+            line = line[0:-len(separator)]  # erase last separator
             line += '\n'
             buffer += line
 
@@ -121,20 +127,20 @@ def replaceInFileTable(filename, inputDir, outputDir, iline, icol, value, mod=0,
 
 
 # TODO: add another dictionary for parameter sweeps?
-def LoFiAeroStruct(tsr,Vel,pitch,R,rho,T,config,options):
+def LoFiAeroStruct(tsr, Vel, pitch, R, rho, T, config, options):
 
     # ======================================================================
     #         Unpack options/params
     # ======================================================================
     path_to_case = options["path_to_case"]
-    outputFile  = options["outputFile"]
+    outputFile = options["outputFile"]
     case_tag = options["case_tag"]
     # casename = options["casename"]
     # spanDir  = options["spanDir"]
-    
-    #TODO: do something with pitch
-    #TODO: do something with rho
-    #TODO: do something with T
+
+    # TODO: do something with pitch
+    # TODO: do something with rho
+    # TODO: do something with T
 
     rotRate_z = tsr * Vel / R
     rpm = rotRate_z / (2 * np.pi) * 60
@@ -146,15 +152,13 @@ def LoFiAeroStruct(tsr,Vel,pitch,R,rho,T,config,options):
     fileDirectory = os.path.join(path_to_case, config["lofi_code"])
     workingDirectory = os.path.join(path_to_case, config["lofi_code"], "workdir")
 
-    shutil.rmtree(workingDirectory,True)
+    shutil.rmtree(workingDirectory, True)
     os.mkdir(workingDirectory)
 
     for file in config["files"]["fileList"]:
-        shutil.copy(os.path.join(fileDirectory,file), os.path.join(workingDirectory,file))
+        shutil.copy(os.path.join(fileDirectory, file), os.path.join(workingDirectory, file))
     for dir in config["files"]["dirList"]:
-        shutil.copytree(os.path.join(fileDirectory,dir), os.path.join(workingDirectory,dir))    
-
-    
+        shutil.copytree(os.path.join(fileDirectory, dir), os.path.join(workingDirectory, dir))
 
     if 'OpenFAST' in config["lofi_code"]:
         # elastodyn: rpm, line 35
@@ -171,22 +175,21 @@ def LoFiAeroStruct(tsr,Vel,pitch,R,rho,T,config,options):
     # ======================================================================
     #         Run OpenFAST / AeroDyn
     # ======================================================================
-    
-    #change to workdir and run
+
+    # change to workdir and run
     cwd = os.getcwd()
     os.chdir(workingDirectory)
 
     flag = os.system(run_cmd)
 
-    if flag!=0:
-        print("ERROR - Execution failed during call to %s"%config["lofi_code"])
+    if flag != 0:
+        print("ERROR - Execution failed during call to %s" %
+              config["lofi_code"])
         exit(1)
-    
-    #go back to where we were
+
+    # go back to where we were
     os.chdir(cwd)
 
-    #Copy the results into the ouput file
+    # Copy the results into the ouput file
     fromdir = os.path.join(workingDirectory, outFile)
     shutil.copy(fromdir, outputFile)
-
-
---
+++ openturbinecode/postprocessing/pp_openfast.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import numpy as np
+
+from fnmatch import fnmatch
+
+from openturbinecode.utils import plotter as plot
+
+# Load the data
+from pCrunch import LoadsAnalysis, FatigueParams
+from pCrunch.io import load_FAST_out
+
+
+from pathlib import Path
+
+
+output_dir = Path("D:/") / "GitHub" / "Solvers" / "openfast" / "IEA15" / "IEA-15-240-RWT-Monopile"
+
+
+def valid_extension(fp):
+    return any([fnmatch(fp, ext) for ext in ["*.outb"]])
+
+
+outfiles = [f for f in output_dir.iterdir() if valid_extension(f)]
+
+outfiles.sort()
+
+print(f"Found {len(outfiles)} output files")
+
+# The new framework provides an object oriented framework to interact with
+# output files. The easiest way to use this is to use the 'load_FAST_out' function.
+
+outputs = load_FAST_out(outfiles[:3])
+print([type(m) for m in outputs])
+
+# An instance of 'OpenFASTBinary' (or 'OpenFASTAscii' if applicable) is created.
+# The instance stores the raw data but also provides many useful methods for
+# interacting with the data:
+
+# print(outputs[0].data)
+# print(outputs[0].time)
+print(outputs[0].channels)
+# print(outputs[0].maxima)
+# print(outputs[0].stddevs)
+
+# Individual channel time series can also be accessed with dict style indexing:
+# outputs[0]["Wind1VelX"]
+
+# Channel magnitudes are defined in a dict:
+magnitude_channels = {
+    "RootMc1": ["RootMxc1", "RootMyc1", "RootMzc1"],
+    "RootMc2": ["RootMxc2", "RootMyc2", "RootMzc2"],
+    "RootMc3": ["RootMxc3", "RootMyc3", "RootMzc3"],
+}
+
+# Define channels (and their fatigue slopes) in a dict:
+fatigue_channels = {
+    "RootMc1": FatigueParams(lifetime=25.0, slope=10.0, ult_stress=6e8),
+    "RootMc2": FatigueParams(lifetime=25.0, slope=10.0, ult_stress=6e8),
+    "RootMc3": FatigueParams(lifetime=25.0, slope=10.0, ult_stress=6e8),
+}
+
+# Define channels to save extreme data in a list:
+channel_extremes = [
+    "RotSpeed",
+    "RotThrust",
+    "RotTorq",
+    "RootMc1",
+    "RootMc2",
+    "RootMc3",
+]
+
+# Convert list of Path objects to list of strings
+outfiles = [str(f) for f in outfiles]
+
+# The API has changed and is in more of an object oriented framework.
+la = LoadsAnalysis(
+    outfiles[:5],                           # The primary input is a list of output files
+    magnitude_channels=magnitude_channels,  # All of the following inputs are optional
+    fatigue_channels=fatigue_channels,      #
+    extreme_channels=channel_extremes,      #
+    trim_data=(0,),                         # If 'trim_data' is passed, all input files will
+)                                           # be trimmed to (tmin, tmax(optional))
+
+la.process_outputs(cores=1,                 # Once LoadsAnalysis is configured, process outputs with
+                   return_damage=True,      # optional return of Palmgren-Miner damage and
+                   goodman=True)            # optional use of goodman correction for mean load values
+
+la.summary_stats
+
+openfast_bin = outputs[0]
+
+
+def get_and_plot_time_series(openfast_bin, channel: str):
+    df: pd.DataFrame = openfast_bin.df
+    unit = str(openfast_bin.units[np.argmax(openfast_bin.channels == channel)])
+    plot_data = df[['Time', channel]].to_numpy()
+    plot.time_series(plot_data, ylabel=f'{channel} [{unit}]', title=f'{channel} Time Series',
+                     filename=f'{output_dir / f"{channel}_Time_Series.svg"}')
+
+
+# Platform Response
+get_and_plot_time_series(openfast_bin, 'PtfmHeave')
+get_and_plot_time_series(openfast_bin, 'PtfmPitch')
+get_and_plot_time_series(openfast_bin, 'PtfmSurge')
+get_and_plot_time_series(openfast_bin, 'PtfmRoll')
+get_and_plot_time_series(openfast_bin, 'PtfmSway')
+get_and_plot_time_series(openfast_bin, 'PtfmYaw')
+
+# Blade 1 Response
+get_and_plot_time_series(openfast_bin, 'BldPitch1')
+get_and_plot_time_series(openfast_bin, 'IPDefl1')
+get_and_plot_time_series(openfast_bin, 'OoPDefl1')
+get_and_plot_time_series(openfast_bin, 'RootFxb1')
+get_and_plot_time_series(openfast_bin, 'RootFyb1')
+get_and_plot_time_series(openfast_bin, 'RootFzb1')
+get_and_plot_time_series(openfast_bin, 'TipDxb1')
+get_and_plot_time_series(openfast_bin, 'TipDyb1')
+get_and_plot_time_series(openfast_bin, 'TipDzb1')
+
+# Rotor Response
+get_and_plot_time_series(openfast_bin, 'RotSpeed')
+get_and_plot_time_series(openfast_bin, 'RotThrust')
+get_and_plot_time_series(openfast_bin, 'RotTorq')
